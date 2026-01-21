33951b4ff2d29ce8d2d807da4e87546d3149aa80
Amit kumar <amitk.kumar414@gmail.com>
2026-01-21 19:18:44 +0530

    edited index.jd

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// index.js (at the root of your project)
 export default function handler(req, res) {
-  // Replace with your actual Twitter username
   res.redirect(307, 'https://x.com/coderespite');
 }
